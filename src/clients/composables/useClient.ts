087b3d1723fabaf28b42d817984dbd6948537399
Joel M. Boada <jmboada30@gmail.com>
2022-12-18 03:32:10 -0400

    add useClient

---
+++ src/clients/composables/useClient.ts
@@ -0,0 +1,26 @@
+import { ref, watch } from 'vue';
+import { useQuery } from '@tanstack/vue-query';
+import clientsApi from '@/api/clients-api';
+import type { Client } from '../interfaces/clients';
+
+const getClient = async (id: number) => {
+  const { data } = await clientsApi.get(`/clients/${id}`);
+  return data;
+};
+
+const useClient = (id: number) => {
+  const client = ref<Client>();
+
+  const { data, isLoading } = useQuery(['client', id], () => getClient(id));
+
+  watch(data, () => {
+    if (data.value) client.value = data.value;
+  }, { immediate: true });
+
+  return {
+    client,
+    isLoading,
+  };
+};
+
+export default useClient;
--- src/clients/pages/ClientPage.vue
+++ src/clients/pages/ClientPage.vue
@@ -1,14 +1,44 @@
-
 <script setup lang="ts">
+import useClient from '@/clients/composables/useClient';
+import LoadingModal from '@/shared/components/LoadingModal.vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
+const {client, isLoading} = useClient(+route.params.id);
 </script>
 
 <template>
-    <div>
-        <h1>Client By Id</h1>
-    </div>
+  <h3>Guardando...</h3>
+  <h3>Guardado</h3>
+
+    <LoadingModal v-if="isLoading" />    
+
+  <div>
+    <h1>Joel Boada</h1>
+
+    <form>
+      <input type="text" placeholder="Nombre" />
+
+      <br />
+
+      <input type="text" placeholder="Direccion" />
+
+      <br />
+      <button type="submit" @click="">Guardar</button>
+    </form>
+  </div>
+
+  <code> {{client}} </code>
 </template>
 
 <style scoped>
+input {
+  width: 100%;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+}
 
+button {
+  margin-bottom: 10px;
+}
 </style>
